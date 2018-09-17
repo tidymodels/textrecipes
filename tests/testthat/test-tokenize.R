bac9e45fb3adaaa47d6ca9fb001095a802885e2a
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2018-09-16 17:12:24 -0700

    better testing for step_tokenize

--- tests/testthat/test-tokenize.R
+++ tests/testthat/test-tokenize.R
@@ -6,8 +6,8 @@ library(textrecipes)
 data(okc_text)
 okc_rec <- recipe(~ ., data = okc_text)
 
-test_that("output is list when length is 1", {
-  data <- tibble(a = rep("a", 20))
+test_that("output is list when length is 1 or 0", {
+  data <- tibble(a = rep(c("a", ""), 20))
   
   data_rec <- recipe(~ ., data = data) %>%
     step_tokenize(a) %>%
@@ -23,22 +23,26 @@ test_that("tokenization is done correctly", {
   
   expect_equal(
     tokenizers::tokenize_words(okc_text$essay0[1]),
-    juice(okc_rec) %>% 
-      slice(1) %>% 
-      pull(essay0)
+    juice(okc_rec) %>% slice(1) %>% pull(essay0)
   )
 })
 
-test_that("tokenization works with other built in tokenizers", {
+test_that("step throws an error if unavaliable tokenizer is picked", {
+  expect_error(
+    okc_rec %>%
+      step_tokenize(essay0, token = "wrong") %>%
+      prep(training = okc_text, retain = TRUE)
+  )
+})
+
+test_that("tokenization works with other built-in tokenizers", {
   okc_rec <- okc_rec %>%
     step_tokenize(essay0, token = "characters") %>%
     prep(training = okc_text, retain = TRUE)
   
   expect_equal(
     tokenizers::tokenize_characters(okc_text$essay0[1]),
-    juice(okc_rec) %>% 
-      slice(1) %>% 
-      pull(essay0)
+    juice(okc_rec) %>% slice(1) %>% pull(essay0)
   )
 })
 
@@ -52,5 +56,16 @@ test_that("tokenization works with custom tokenizer", {
     juice(okc_rec) %>% 
       slice(1) %>% 
       pull(essay0)
+  )
+})
+
+test_that("arguments are passed using options argument", {
+  okc_rec <- okc_rec %>%
+    step_tokenize(essay0, options = list(lowercase = FALSE)) %>%
+    prep(training = okc_text, retain = TRUE)
+  
+  expect_equal(
+    tokenizers::tokenize_words(okc_text$essay0[1], lowercase = FALSE),
+    juice(okc_rec) %>% slice(1) %>% pull(essay0)
   )
 })
