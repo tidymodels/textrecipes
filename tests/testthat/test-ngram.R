e9aa51e85bff73dcbaf0042ae3e63b4442fcf847
EmilHvitfeldt <emilhhvitfeldt@gmail.com>
2020-04-04 20:35:12 -0700

    add step_ngram

--- tests/testthat/test-ngram.R
+++ tests/testthat/test-ngram.R
@@ -64,3 +64,76 @@ test_that("ngram returns length zero vectors when length(x) < n", {
 })
 
 
+library(recipes)
+library(textrecipes)
+
+test_tibble <- tibble(text = c("not eat them here or there.",
+                               "not eat them anywhere.")
+)
+
+rec <- recipe(~ ., data = test_tibble)
+
+test_that("ngramming is done correctly", {
+  rec <- rec %>%
+    step_tokenize(text) %>%
+    step_ngram(text) 
+  
+  obj <- rec %>%
+    prep(training = test_tibble, retain = TRUE)
+  
+  expect_equal(
+    juice(obj) %>% 
+      pull(text) %>%
+      vctrs::vec_data(),
+    list(c("not_eat_them", "eat_them_here", "them_here_or", "here_or_there"),
+         c("not_eat_them", "eat_them_anywhere"))
+  )
+  
+  expect_equal(dim(tidy(rec, 2)), c(1, 3))
+  expect_equal(dim(tidy(obj, 2)), c(1, 2))
+})
+
+test_that("`n` argument works", {
+  rec <- rec %>%
+    step_tokenize(text) %>%
+    step_ngram(text, n = 2) 
+  
+  obj <- rec %>%
+    prep(training = test_tibble, retain = TRUE)
+  
+  expect_equal(
+    juice(obj) %>% 
+      pull(text) %>%
+      vctrs::vec_data(),
+    list(c("not_eat", "eat_them", "them_here", "here_or", "or_there"),
+         c("not_eat", "eat_them", "them_anywhere"))
+  )
+})
+
+test_that("`delim` argument works", {
+  rec <- rec %>%
+    step_tokenize(text) %>%
+    step_ngram(text, delim = " ") 
+  
+  obj <- rec %>%
+    prep(training = test_tibble, retain = TRUE)
+  
+  expect_equal(
+    juice(obj) %>% 
+      pull(text) %>%
+      vctrs::vec_data(),
+    list(c("not eat them", "eat them here", "them here or", "here or there"),
+         c("not eat them", "eat them anywhere"))
+  )
+})
+
+
+test_that("printing", {
+  rec <- rec %>%
+    step_tokenize(text) %>%
+    step_stem(text)
+  expect_output(print(rec))
+  expect_output(prep(rec, training = test_tibble, verbose = TRUE))
+})
+
+
