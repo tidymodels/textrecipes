48b7e7d91f58da63922955301bc938b0e07cdedc
Julia Silge <julia.silge@gmail.com>
2020-09-17 18:10:14 -0600

    Initial go at step_clean_levels()

--- R/0_imports.R
+++ R/0_imports.R
@@ -1,14 +1,14 @@
 #' @importFrom recipes add_step step terms_select sel2char ellipse_check names0
 #' @importFrom recipes printer check_type rand_id bake prep names0 is_trained
 #' 
-#' @importFrom tibble as_tibble tibble
+#' @importFrom tibble as_tibble tibble enframe
 #' 
 #' @importFrom purrr map map_dfc map_chr map_lgl map_dfr pmap keep
 #' 
 #' @importFrom rlang %||% na_chr na_int expr na_lgl .data
 #' 
 #' @importFrom dplyr bind_cols inner_join mutate_all select rename_all 
-#' @importFrom dplyr summarize_all
+#' @importFrom dplyr summarize_all recode_factor
 #' 
 #' @importFrom generics tidy
 #' 
