e535fbac22207f5be062b690d9abb19654ecbb36
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2022-01-03 17:47:29 -0800

    don't import ellipse_check

--- R/0_imports.R
+++ R/0_imports.R
@@ -1,7 +1,7 @@
 #' @importFrom generics tidy
 #' @importFrom Matrix sparseMatrix
 #' @importFrom purrr keep map map_chr map_dfc map_dfr map_lgl pmap
-#' @importFrom recipes add_step bake check_type ellipse_check is_trained names0
+#' @importFrom recipes add_step bake check_type is_trained names0
 #' @importFrom recipes prep printer rand_id sel2char step recipes_eval_select
 #' @importFrom rlang .data %||% enquos expr na_chr na_int na_lgl :=
 #' @importFrom tibble as_tibble enframe tibble
