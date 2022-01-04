17df515ef49fc9a7a812b171a94bf064f6732359
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2022-01-03 17:35:12 -0800

    empty selector step_tokenfilter

--- tests/testthat/test-tokenfilter.R
+++ tests/testthat/test-tokenfilter.R
@@ -1,5 +1,3 @@
-context("test-tokenfilter")
-
 library(textrecipes)
 library(recipes)
 
@@ -97,4 +95,45 @@ test_that("printing", {
   expect_warning(
     expect_output(prep(rec, verbose = TRUE))
   )
+})
+
+test_that("empty selection prep/bake is a no-op", {
+  rec1 <- recipe(mpg ~ ., mtcars)
+  rec2 <- step_tokenfilter(rec1)
+  
+  rec1 <- prep(rec1, mtcars)
+  rec2 <- prep(rec2, mtcars)
+  
+  baked1 <- bake(rec1, mtcars)
+  baked2 <- bake(rec2, mtcars)
+  
+  expect_identical(baked1, baked1)
+})
+
+test_that("empty selection tidy method works", {
+  rec <- recipe(mpg ~ ., mtcars)
+  rec <- step_tokenfilter(rec)
+  
+  expect_identical(
+    tidy(rec, number = 1),
+    tibble(terms = character(), value = integer(), id = character())
+  )
+  
+  rec <- prep(rec, mtcars)
+  
+  expect_identical(
+    tidy(rec, number = 1),
+    tibble(terms = character(), value = integer(), id = character())
+  )
+})
+
+test_that("empty printing", {
+  rec <- recipe(mpg ~ ., mtcars)
+  rec <- step_tokenfilter(rec)
+  
+  expect_snapshot(rec)
+  
+  rec <- prep(rec, mtcars)
+  
+  expect_snapshot(rec)
 })
