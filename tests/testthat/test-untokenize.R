c8f2b0049d8d5752a785045db5f5938bc613b715
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2022-01-03 17:46:51 -0800

    empty selectors step_untokenize

--- tests/testthat/test-untokenize.R
+++ tests/testthat/test-untokenize.R
@@ -1,5 +1,3 @@
-context("test-untokenize")
-
 library(recipes)
 library(textrecipes)
 
@@ -72,4 +70,45 @@ test_that("printing", {
     step_untokenize(text)
   expect_output(print(rec))
   expect_output(prep(rec, verbose = TRUE))
+})
+
+test_that("empty selection prep/bake is a no-op", {
+  rec1 <- recipe(mpg ~ ., mtcars)
+  rec2 <- step_untokenize(rec1)
+  
+  rec1 <- prep(rec1, mtcars)
+  rec2 <- prep(rec2, mtcars)
+  
+  baked1 <- bake(rec1, mtcars)
+  baked2 <- bake(rec2, mtcars)
+  
+  expect_identical(baked1, baked1)
+})
+
+test_that("empty selection tidy method works", {
+  rec <- recipe(mpg ~ ., mtcars)
+  rec <- step_untokenize(rec)
+  
+  expect_identical(
+    tidy(rec, number = 1),
+    tibble(terms = character(), value = character(), id = character())
+  )
+  
+  rec <- prep(rec, mtcars)
+  
+  expect_identical(
+    tidy(rec, number = 1),
+    tibble(terms = character(), value = character(), id = character())
+  )
+})
+
+test_that("empty printing", {
+  rec <- recipe(mpg ~ ., mtcars)
+  rec <- step_untokenize(rec)
+  
+  expect_snapshot(rec)
+  
+  rec <- prep(rec, mtcars)
+  
+  expect_snapshot(rec)
 })
