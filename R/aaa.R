497706dd68d5c352ecf967177b8302d7c4632254
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2023-08-16 12:39:05 -0700

    remove is_cran_check()

--- R/aaa.R
+++ R/aaa.R
@@ -38,17 +38,3 @@ table0 <- function(x) {
 
   purrr::set_names(res$n, res$tokens)
 }
-
-# adapted from ps:::is_cran_check()
-is_cran_check <- function() {
-  # We are using this because data.table uses all cores by default, and CRAN
-  # doesn't like it
-  return(TRUE)
-  
-  if (identical(Sys.getenv("NOT_CRAN"), "true")) {
-    FALSE
-  }
-  else {
-    Sys.getenv("_R_CHECK_PACKAGE_NAME_", "") != ""
-  }
-}
